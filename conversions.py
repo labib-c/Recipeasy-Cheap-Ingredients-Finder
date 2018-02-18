14db734c77d32d2907ebeb3e54c9ba2f9fa9cdf2
Labib <labibc01@gmail.com>
2018-02-18 01:09:13 -0500

    added conversion equations to satisfy our units

---
+++ conversions.py
@@ -0,0 +1,20 @@
+def tablespoons_to_100ml(tbsp):
+    return (tbsp*15) / 100
+
+
+def teaspoon_to_100ml(tsp):
+    return (tsp*15) / 100
+
+
+def cups_to_100ml(cups):
+    return (cups*225) / 100
+
+
+def pound_to_100g(pounds):
+    return (pounds*450) / 100
+
+
+def ounce_to_100g(ounces):
+    return (ounces*30) / 100
+
+
